9120ef1732115b45f088362dc7450fbe4257847a
Umid <suleymanli4240@gmail.com>
2026-02-28 21:20:15 +0400

    Create core package and main classes

---
+++ core/RingBuffer.java
@@ -0,0 +1,14 @@
+package core;
+
+public class RingBuffer<T> {
+    public RingBuffer(int capacity) {
+    }
+
+    public Writer<T> createWriter() {
+        return null;
+    }
+
+    public Reader<T> createReader() {
+        return null;
+    }
+}
