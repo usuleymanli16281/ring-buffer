71a57d96d10adedb269dbd6deac80c3b12107162
Umid <suleymanli4240@gmail.com>
2026-02-28 21:58:39 +0400

    Implemented write logic

--- core/Writer.java
+++ core/Writer.java
@@ -1,4 +1,15 @@
 package core;
 
 public class Writer<T> {
+    private final RingBuffer<T> buffer;
+
+    Writer(RingBuffer<T> buffer) {
+        this.buffer = buffer;
+    }
+
+    public void write(T item) {
+        long sequence = buffer.currentWriteSequence();
+        buffer.putAt(sequence, item);
+        buffer.advanceWriteSequence();
+    }
 }
