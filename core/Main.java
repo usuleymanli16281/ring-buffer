2661aba991470a90bf3c1e969e4864361fbfeb7b
Umid <suleymanli4240@gmail.com>
2026-03-01 17:19:25 +0400

    Added implementation of main class and added test case

--- core/Main.java
+++ core/Main.java
@@ -2,5 +2,35 @@ package core;
 
 public class Main {
     public static void main(String[] args) {
+        RingBuffer<Integer> buffer = new RingBuffer<>(3);
+
+        Writer<Integer> writer = buffer.createWriter();
+
+        Reader<Integer> r1 = buffer.createReader();
+        Reader<Integer> r2 = buffer.createReader();
+
+        writer.write(20);
+        writer.write(40);
+        writer.write(60);
+
+        System.out.println("r1: " + r1.read());
+        System.out.println("r2: " + r2.read());
+
+        writer.write(80);
+        writer.write(100);
+
+        System.out.println("r1: " + r1.read());
+        System.out.println("r1: " + r1.read());
+
+        System.out.println("r2: " + r2.read());
+        System.out.println("r2 missed: " + r2.getMissedCount());
+
+        try {
+            buffer.createWriter();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println("reader count: " + buffer.getReaderCount());
     }
 }
